c346574757bca105a2ae92383cb08ee83770740a
GabrielDvPereira <gdavi.gd@gmail.com>
2022-08-25 22:12:18 -0300

    feat: add i2c lib to the project

---
+++ src/i2c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "bme280.h"
+
+void initTempetureSensor(){
+    struct bme280_dev dev;
+    int8_t rslt = BME280_OK;
+    uint8_t dev_addr = BME280_I2C_ADDR_PRIM;
+
+    dev.intf_ptr = &dev_addr;
+    dev.intf = BME280_I2C_INTF;
+    dev.read = user_i2c_read;
+    dev.write = user_i2c_write;
+    dev.delay_ms = user_delay_ms;
+
+    rslt = bme280_init(&dev);
+}
