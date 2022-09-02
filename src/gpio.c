733aa137a42f1ac53cc7d43bb04eb65b64d56115
GabrielDvPereira <gdavi.gd@gmail.com>
2022-09-01 23:48:49 -0300

    feat: add gpio handler

---
+++ src/gpio.c
@@ -0,0 +1,44 @@
+#include <wiringPi.h>
+#include <softPwm.h>
+#include "gpio.h"
+
+void initialize_gpio(){
+  wiringPiSetup();
+  softPwmCreate(FAN_GPIO_PIN, PWM_MIN, PWM_MAX);
+  softPwmCreate(RESISTOR_GPIO_PIN, PWM_MIN, PWM_MAX);
+}
+
+
+void update_fan_power(float pwm){
+    if(pwm < FAN_MIN){
+        pwm = FAN_MIN;
+    }
+    softPwmWrite(FAN_GPIO_PIN, (int) pwm);
+}
+
+void update_resistor_power(float pwm){
+    softPwmWrite(FAN_GPIO_PIN, (int) pwm);
+}
+
+void turn_off_fan(){
+    softPwmWrite(FAN_GPIO_PIN, PWM_MIN);
+}
+void turn_off_resistor(){
+    softPwmWrite(RESISTOR_GPIO_PIN, PWM_MIN);
+}
+
+
+void adjust_tempeture(float pwm){
+    if(pwm == 0){
+        turn_off_fan();
+        turn_off_resistor(); 
+        return;
+    }
+
+    if(pwm > 0){
+        update_resistor_power(pwm);
+        return;
+    }
+
+    update_fan_power((-1)*pwm);
+}
